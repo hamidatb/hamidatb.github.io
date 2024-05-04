13ad063cd614312520cd464cdb88cab0fd16befd
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-05-03 18:23:15 -0600

    Updating Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+/* Using to navigate to other parts of the page*/
+import { Link } from "react-router-dom";  
+
+import { styles } from '../styles';
+import { navLinks } from '../constants/constants';
+import { logo, menu, close } from '../assets';
+
 
 const Navbar = () => {
   return (
