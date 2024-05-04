470fe934532812bb2f55dfd0268f2b96b7967098
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-05-03 19:03:33 -0600

    Updating the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,39 @@ import { logo, menu, close } from '../assets';
 
 
 const Navbar = () => {
+  const [active, SetActive] = useState("");
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}
+      /* This is supposed to be className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`} -> Note the word fixed */
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" 
+        onClick={() => 
+          {SetActive("");
+          window.scrollTo(0,0);
+          }}
+        >
+          <img src={logo} aly="logo" className="w-9 h-9 object-contain" />
+          <p className=" text-white text-[18px] font-bold cursor-pointer"> Hamidat Bello <span className="sm:block hidden">| Software Devloper</span></p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((Link) => (
+            <li 
+              key={Link.id} 
+              className={`${ active === Link.title 
+              ? "text-white" 
+              : "text-secondary" }
+              hover:text-white text-[18px] font-medium cursor-pointer`}
+              >
+              <a href={'#${Link.id}'}>{Link.title}</a>
+            </li>
+          ))}
+
+        </ul>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
