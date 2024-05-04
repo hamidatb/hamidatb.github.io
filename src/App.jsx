6a9094c876ae394152da0e209d413773679a81be
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-05-04 00:30:28 -0600

    Launching to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { BrowserRouter } from "react-router-dom"; // Using BrowserRouter for routing
+import { HashRouter as Router } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      {/* Container for all page components with a background setup */}
+    <Router>
       <div className="relative z-0 bg-primary">
-        {/* Section containing the Navbar and Hero components with specific background styling */}
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar /> 
           <Hero />
         </div>
-        <About />      
-        <Experience /> 
-        <Works />      
+        <About />
+        <Experience />
+        <Works />
         <Tech />
         <div className="relative z-0">
-          <Contact />    {/* Contact section for visitors to reach out :) */}
-          <StarsCanvas />{/* The visual starry background */}
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
