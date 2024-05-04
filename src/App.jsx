678e8db60ae1ee6d337184564957be10ffb59273
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-05-03 18:16:14 -0600

    Updating App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom"; // Using BrowserRouter for routing
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      {/* Container for all page components with a background setup */}
+      <div className="relative z-0 bg-primary">
+        {/* Section containing the Navbar and Hero components with specific background styling */}
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar /> 
+          <Hero />
+        </div>
+        <About />      
+        <Experience /> 
+        <Works />      
+        <Tech />      
+        <div className="relative z-0">
+          <Contact />    {/* Contact section for visitors to reach out :) */}
+          <StarsCanvas />{/* The visual starry background */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
