2a5646f18dabf1f8647580c450d6ab8fc81cd881
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-05-15 15:25:00 -0700

    Resize computer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-10`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -18,18 +18,18 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Hamidat</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-           <br className='sm:block hidden' />
-           👋🏾 I'm a future-driven Computing Science student at the University of Alberta, dedicated to creating software solutions that make a difference. 
-           Dive into my portfolio to see how I blend innovation with purpose.
-          </p>
-          
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            <br className='sm:block hidden' />
+            👋🏾 I'm a future-driven Computing Science student at the University of Alberta, dedicated to creating software solutions that make a difference. 
+            Dive into my portfolio to see how I blend innovation with purpose.
+            </p>
+            
         </div>
       </div>
 
-      
-      <ComputersCanvas className="mt-12" /> {/* Increase top margin - Tailwind */}
 
+      <ComputersCanvas />
+  
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
